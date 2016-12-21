8c0534cd52016c3c9e3d5b61404cd3ad7b9ed872
arveh4 <arveh4@outlook.com>
2016-12-22 00:27:25 +0100

    Refactored TestIndexes

--- ACopyLibTest/IntegrationTests/TestIndexes.cs
+++ ACopyLibTest/IntegrationTests/TestIndexes.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using ACopyLib.U4Indexes;
 using ACopyLib.Xml;
 using ADatabase;
@@ -28,8 +27,8 @@ namespace ACopyLibTest.IntegrationTests
         {
             DbSchema.DropTable(Asysindex);
             DbSchema.DropTable(Aagindex);
-            IColumnFactory columnFactory = DbContext.PowerPlant.CreateColumnFactory();
-            List<IColumn> columns = new List<IColumn>
+            var columnFactory = DbContext.PowerPlant.CreateColumnFactory();
+            var columns = new List<IColumn>
             { 
                 columnFactory.CreateInstance(ColumnType.String, "index_name", 60, false, "' '", "Danish_Norwegian_CI_AS"),
                 columnFactory.CreateInstance(ColumnType.String, "table_name", 60, false, "' '", "Danish_Norwegian_CI_AS"),
@@ -44,25 +43,25 @@ namespace ACopyLibTest.IntegrationTests
 
         private void InsertIntoIndexesTable(string indexesTable, string indexName, string columnList)
         {
-            string insertStmt = String.Format("insert into {0} (index_name, table_name, column_list, location_name, unique_flag, db_name) values ('{1}', '{2}', '{3}', 'default', 0, 'ORACLE')", 
-                indexesTable, indexName, TestTable, columnList);
+            string insertStmt =
+                $"insert into {indexesTable} (index_name, table_name, column_list, location_name, unique_flag, db_name) values ('{indexName}', '{TestTable}', '{columnList}', 'default', 0, 'ORACLE')";
             Commands.ExecuteNonQuery(insertStmt);
         }
 
         private void CreateTestTableWithIndex()
         {
-            IColumnFactory columnFactory = DbContext.PowerPlant.CreateColumnFactory();
-            List<IColumn> columns = new List<IColumn>
+            var columnFactory = DbContext.PowerPlant.CreateColumnFactory();
+            var columns = new List<IColumn>
             { 
                 columnFactory.CreateInstance(ColumnType.Int64, "id", false, "0"),
                 columnFactory.CreateInstance(ColumnType.Int64, "id2", false, "0"),
                 columnFactory.CreateInstance(ColumnType.Varchar, "val", 50, false, "' '", "Danish_Norwegian_CI_AS") 
             };
-            TableDefinition tableDefinition = new TableDefinition(TestTable, columns, "");
+            var tableDefinition = new TableDefinition(TestTable, columns, "");
             DbSchema.CreateTable(tableDefinition);
-            string tmp = string.Format("insert into {0} (id, val) values (9, 'control value')", TestTable);
+            string tmp = $"insert into {TestTable} (id, val) values (9, 'control value')";
             Commands.ExecuteNonQuery(tmp);
-            Commands.ExecuteNonQuery(string.Format("create unique index {0} on {1}(id)", "i_" + TestTable, TestTable));
+            Commands.ExecuteNonQuery($"create unique index {"i_" + TestTable} on {TestTable}(id)");
         }
 
         // TestMethod
@@ -77,8 +76,8 @@ namespace ACopyLibTest.IntegrationTests
 
         private IXmlSchema SetupXmlSchemaAndTable()
         {
-            IXmlSchema xmlSchema = XmlSchemaFactory.CreateInstance(DbContext);
-            IU4Indexes u4Indexes = U4IndexesFactory.CreateInstance(DbContext);
+            var xmlSchema = XmlSchemaFactory.CreateInstance(DbContext);
+            var u4Indexes = U4IndexesFactory.CreateInstance(DbContext);
             u4Indexes.AagTableName = Aagindex;
             u4Indexes.AsysTableName = Asysindex;
             xmlSchema.U4Indexes = u4Indexes;
@@ -98,18 +97,18 @@ namespace ACopyLibTest.IntegrationTests
         private ITableDefinition WriteAndReadSchema(IXmlSchema xmlSchema)
         {
             xmlSchema.Write(".\\", TestTable, "aschema");
-            ITableDefinition tableDefinition = xmlSchema.GetTableDefinition(".\\" + TestTable + ".aschema");
+            var tableDefinition = xmlSchema.GetTableDefinition(".\\" + TestTable + ".aschema");
             return tableDefinition;
         }
 
         // TestMethod
         protected void TestIndexes_When_IndexExistsInAsysIndex()
         {
-            IXmlSchema xmlSchema = SetupXmlSchemaAndTable();
+            var xmlSchema = SetupXmlSchemaAndTable();
 
             InsertIntoIndexesTable(Asysindex, "i_" + TestTable + "1", "id, val");
 
-            ITableDefinition tableDefinition = WriteAndReadSchema(xmlSchema);
+            var tableDefinition = WriteAndReadSchema(xmlSchema);
 
             tableDefinition.Indexes.Count.Should().Be(2, "because an extra index was added from asysindex");
 
@@ -119,12 +118,12 @@ namespace ACopyLibTest.IntegrationTests
         // TestMethod
         protected void TestIndexes_When_IndexExistsInBothAagAndAsysIndex_Then_OnlyAAgAdded()
         {
-            IXmlSchema xmlSchema = SetupXmlSchemaAndTable();
+            var xmlSchema = SetupXmlSchemaAndTable();
 
             InsertIntoIndexesTable(Asysindex, "i_" + TestTable + "1", "id, val");
             InsertIntoIndexesTable(Aagindex, "i_" + TestTable + "1", "id2, val");
 
-            ITableDefinition tableDefinition = WriteAndReadSchema(xmlSchema);
+            var tableDefinition = WriteAndReadSchema(xmlSchema);
 
             tableDefinition.Indexes.Count.Should().Be(2, "because index in aag should override asys (when indexes has same name)");
             tableDefinition.Indexes[1].Columns[0].Name.Should().Be("id2", "because index with id from asys should be overridden");
@@ -135,11 +134,11 @@ namespace ACopyLibTest.IntegrationTests
         // TestMethod
         protected void TestFunctionBasedIndex()
         {
-            IXmlSchema xmlSchema = SetupXmlSchemaAndTable();
+            var xmlSchema = SetupXmlSchemaAndTable();
 
             InsertIntoIndexesTable(Asysindex, "i_" + TestTable + "1", "to_number(to_char(id)||''00'')");
 
-            ITableDefinition tableDefinition = WriteAndReadSchema(xmlSchema);
+            var tableDefinition = WriteAndReadSchema(xmlSchema);
 
             tableDefinition.Indexes.Count.Should().Be(2, "because index we have an index in asysindex in addition");
             tableDefinition.Indexes[1].Columns[0].Name.Should().Be("expression", "because index is function based");
@@ -150,11 +149,11 @@ namespace ACopyLibTest.IntegrationTests
         // TestMethod
         protected void TestIndexes_When_SameIndexInAagIndexAndOnTable_Then_OnTableWins()
         {
-            IXmlSchema xmlSchema = SetupXmlSchemaAndTable();
+            var xmlSchema = SetupXmlSchemaAndTable();
 
             InsertIntoIndexesTable(Asysindex, "i_" + TestTable, "id2");
 
-            ITableDefinition tableDefinition = WriteAndReadSchema(xmlSchema);
+            var tableDefinition = WriteAndReadSchema(xmlSchema);
 
             tableDefinition.Indexes.Count.Should().Be(1, "because index on table should override aagindex");
             tableDefinition.Indexes[0].Columns[0].Name.Should().Be("id", "because index on table should win");
